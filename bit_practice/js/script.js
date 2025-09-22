d30bbe9cb394e57a7b56149ca08f393c7d8e2f85
Issy64 <isikoron64@gmail.com>
2025-09-22 22:35:31 +0900

    bit_practice

--- bit_practice/js/script.js
+++ bit_practice/js/script.js
@@ -1,24 +1,45 @@
 const colorPicker = document.querySelector("#colorPicker");
 const colorText = document.querySelector("#colorText");
 const pickingBackgroundColor = document.querySelector(".picking");
-// console.log("test");
-
-// colorText.textContent = "change example";
-// console.log(colorPicker.value);
-// colorText.textContent = colorPicker.value;
+const colorBitRed = document.querySelector("#colorBitRed");
+const colorBitBlue = document.querySelector("#colorBitBlue");
+const colorBitGreen = document.querySelector("#colorBitGreen");
 
 colorPicker.addEventListener("input", () => {
-    // console.log(colorPicker.value);
-    let hexToBinary = 0;
-    hexToBinary = colorPicker.value;
-    hexToBinary = hexToBinary.substr(1);
-    // console.log(parseInt(hexToBinary, 16));
-    hexToBinary = parseInt(hexToBinary,16).toString(2);
-    console.log(hexToBinary);
+    let hexToBinary = colorPicker.value.substr(1);
+    //取得したカラーコードの16進数文字列を2進数文字列に変換する。
+    //24bitのままだと桁ズレが怖いので8bitずつ処理する。
+    //もう少し簡単な記述はなかったものか…
+    const redBitBin = parseInt(hexToBinary.slice(0, 2), 16).toString(2).padStart(8, "0");
+    const blueBitBin = parseInt(hexToBinary.slice(2, 4), 16).toString(2).padStart(8, "0");
+    const greenBitBin = parseInt(hexToBinary.slice(4, 6), 16).toString(2).padStart(8, "0");
+    // console.log(redBitBin);
+    // console.log(blueBitBin);
+    // console.log(greenBitBin);
+    const colorBitBin = redBitBin + " " + blueBitBin + " " + greenBitBin;
 
-    // const binaryRed = ((hexToBinary >> 16) & 0xff);
-    // console.log(binaryRed);
-
-    colorText.textContent = hexToBinary;
+    colorText.textContent = colorBitBin;
     pickingBackgroundColor.style.backgroundColor = colorPicker.value;
+    colorBitRed.textContent = redBitBin;
+    colorBitBlue.textContent = blueBitBin;
+    colorBitGreen.textContent = greenBitBin;
+});
+
+const inputMask = document.querySelector("#inputMask");
+const maskBitRed = document.querySelector("#maskBitRed");
+const maskBitBlue = document.querySelector("#maskBitBlue");
+const maskBitGreen = document.querySelector("#maskBitGreen");
+
+inputMask.addEventListener("input", () => {
+    const maskBin = inputMask.value;
+    console.log(maskBin);
+    const maskRed = maskBin.slice(0, 8);
+    const maskBlue = maskBin.slice(8, 16);
+    const maskGreen = maskBin.slice(16, 24);
+    console.log(maskRed);
+    console.log(maskBlue);
+    console.log(maskGreen);
+    maskBitRed.textContent = maskRed;
+    maskBitBlue.textContent = maskBlue;
+    maskBitGreen.textContent = maskGreen;
 });
